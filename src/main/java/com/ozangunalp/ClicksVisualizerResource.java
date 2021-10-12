bc19ef3e2ab551ffe4ea53fa62b09313ed180346
Ozan Gunalp <ozangunalp@gmail.com>
2021-10-12 16:21:08 +0100

    Reactive messaging incoming channel

--- src/main/java/com/ozangunalp/ClicksVisualizerResource.java
+++ src/main/java/com/ozangunalp/ClicksVisualizerResource.java
@@ -1,42 +1,33 @@
 package com.ozangunalp;
 
+import java.time.Duration;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.sse.OutboundSseEvent;
-import javax.ws.rs.sse.Sse;
-import javax.ws.rs.sse.SseBroadcaster;
-import javax.ws.rs.sse.SseEventSink;
+
+import org.eclipse.microprofile.reactive.messaging.Channel;
+
+import io.smallrye.mutiny.Multi;
 
 @ApplicationScoped
 @Path("/clicks")
 public class ClicksVisualizerResource {
 
-    private final OutboundSseEvent.Builder eventBuilder;
-    private final SseBroadcaster broadcaster;
-
-    public ClicksVisualizerResource(@Context Sse sse) {
-        this.eventBuilder = sse.newEventBuilder();
-        this.broadcaster = sse.newBroadcaster();
-    }
+    @Channel("clicks")
+    Multi<PointerEvent> clicks;
 
     @GET
     @Produces(MediaType.SERVER_SENT_EVENTS)
-    public void stream(@Context SseEventSink eventSink) {
-        broadcaster.register(eventSink);
+    public Multi<PointerEvent> stream() {
+        return Multi.createBy().merging()
+                .streams(clicks, emitAPeriodicPing());
     }
 
-    public void sendEvent(String topic, PointerEvent event) {
-        this.eventBuilder
-                .id(event.userId)
-                .data(PointerEvent.class, event)
-                .name(topic)
-                .mediaType(MediaType.APPLICATION_JSON_TYPE)
-                .reconnectDelay(3000);
-        OutboundSseEvent sseEvent = eventBuilder.build();
-        broadcaster.broadcast(sseEvent);
+    Multi<PointerEvent> emitAPeriodicPing() {
+        return Multi.createFrom().ticks().every(Duration.ofSeconds(10))
+                .onItem().transform(x -> new PointerEvent());
     }
 }
