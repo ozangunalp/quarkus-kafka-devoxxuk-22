41da4ce07e30805b6c577f6cdff84fdfd90e71d2
Ozan Gunalp <ozangunalp@gmail.com>
2021-10-13 09:41:13 +0100

    Persist incoming messages using Kafka batches

--- src/main/java/com/ozangunalp/ClicksPersistence.java
+++ src/main/java/com/ozangunalp/ClicksPersistence.java
@@ -1,6 +1,5 @@
 package com.ozangunalp;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,11 +7,8 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.Message;
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.jboss.logging.Logger;
 
-import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import io.vertx.mutiny.pgclient.PgPool;
 import io.vertx.mutiny.sqlclient.Tuple;
@@ -27,23 +23,8 @@ public class ClicksPersistence {
     PgPool pgClient;
 
     @Incoming("clicks")
-    @Outgoing("clicks-ignored")
-    public Multi<Message<PointerEvent>> clicks(Multi<Message<PointerEvent>> clicks) {
-        return clicks.group().intoLists().of(20, Duration.ofSeconds(5))
-                .onItem().transformToMultiAndConcatenate(messages ->
-                        insertBatch(messages.stream().map(Message::getPayload).collect(Collectors.toList()))
-                                .onFailure().retry().atMost(1)
-                                .onFailure().recoverWithUni(throwable ->
-                                        Uni.combine().all().unis(messages.stream()
-                                                        .map(m -> Uni.createFrom().completionStage(m.nack(throwable)))
-                                                        .collect(Collectors.toList()))
-                                                .discardItems())
-                                .onItem().transformToMulti(unused -> Multi.createFrom().iterable(messages)));
-    }
-
-    @Incoming("clicks-ignored")
-    public void ignores(PointerEvent ignored) {
-
+    public Uni<Void> clicks(List<PointerEvent> events) {
+        return insertBatch(events);
     }
 
     private static final String sql =
